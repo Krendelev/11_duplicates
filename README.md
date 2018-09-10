f84e353504becab0d6db2edc4c3139eaccd54855
Krendelev <krendelev@gmail.com>
2018-09-11 02:39:54 +0300

    implement main feature

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+Simple script that finds all the duplicates (e.g. files with the same name and size) in directory provided.
 
-# Project Goals
+## Quick start
+
+```bash
+$ python duplicates.py test_dir
+Duplicates found:
+test_dir/1/file2
+test_dir/4/file2
+test_dir/3/file2
+
+test_dir/1/file1
+test_dir/4/file1
+```
+
+## Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,36 @@
+import os
+import sys
+
+
+def get_duplicates(path):
+    file_dict, duplicates = {}, {}
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            file_path = os.path.join(root, name)
+            file_size = os.path.getsize(file_path)
+            dup_location = file_dict.setdefault((name, file_size), [])
+            if dup_location:
+                duplicates.update({name: dup_location})
+            dup_location.append(root)
+    return duplicates
+
+
+def print_duplicates(duplicates):
+    if duplicates:
+        print('Duplicates found:')
+        for name, paths in duplicates.items():
+            output = ['/'.join([path, name]) for path in paths]
+            print(*output, sep='\n', end='\n\n')
+    else:
+        print('Duplicates not found')
+
 
 if __name__ == '__main__':
-    pass
+    try:
+        path = sys.argv[1]
+    except IndexError:
+        print('Please specify directory name')
+    if os.path.isdir(path):
+        print_duplicates(get_duplicates(path))
+    else:
+        print('Not a directory')
