d8ef2f132fd5f1c17b325297a41e2c39f97a3f93
Krendelev <krendelev@gmail.com>
2018-09-12 17:07:13 +0300

    change names for added consistency

--- duplicates.py
+++ duplicates.py
@@ -2,29 +2,28 @@ import os
 import sys
 
 
-def get_files(path):
-    file_dict = {}
+def get_files_locations(path):
+    locations = {}
     for root, _, filenames in os.walk(path):
         for filename in filenames:
             file_path = os.path.join(root, filename)
             file_size = os.path.getsize(file_path)
-            file_dict_insertion = file_dict.setdefault(
+            file_dict_insertion = locations.setdefault(
                 (filename, file_size),
                 []
                 ).append(file_path)
-    return file_dict
+    return locations
 
 
-def print_duplicates(file_list):
+def print_duplicates(files_locations):
     duplicates = [
-        path for paths in file_list.values()
+        path for paths in files_locations.values()
         for path in paths if len(paths) > 1
     ]
     if duplicates:
         print('Duplicates found:', *duplicates, sep='\n')
     else:
         print('Duplicates not found')
-    return None
 
 
 if __name__ == '__main__':
@@ -33,6 +32,6 @@ if __name__ == '__main__':
     except IndexError:
         print('Please specify directory name')
     if os.path.isdir(path):
-        print_duplicates(get_files(path))
+        print_duplicates(get_files_locations(path))
     else:
         print('{} is not a directory'.format(path))
